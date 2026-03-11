51b0e058b6d29ad84b622ac794377138414fe096
Pooja sri S <spoojasri9493@gmail.com>
2026-03-11 20:21:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,13 @@
 import boto3
 
-AWS_ACCESS_KEY_ID = "YOUR_ACCESS_KEY"
-AWS_SECRET_ACCESS_KEY = "YOUR_SECRET_KEY"
 AWS_REGION = "ap-south-1"
 
 dynamodb = boto3.resource(
     "dynamodb",
-    region_name=AWS_REGION,
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    region_name=AWS_REGION
 )
 
 users_table = dynamodb.Table("users")
 bookings_table = dynamodb.Table("bookings")
 reviews_table = dynamodb.Table("reviews")
+
